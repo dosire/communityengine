a7de8ad0a1269ceb3f8deaf7af33a0869a445d45
dosire <sytse@comcoaster.com>
2008-09-29 08:29:06 +0200

    Allow countries without metro area's to be selected.

--- app/models/country.rb
+++ app/models/country.rb
@@ -13,6 +13,11 @@ class Country < ActiveRecord::Base
     MetroArea.find(:all, :include => :country).collect{ |m| m.country }.sort_by{ |c| c.name }.uniq
   end
   
+  #Add new function to also select countries without metros
+  def self.find_countries_with_and_without_metros
+    Country.find(:all)
+  end
+  
   def states
     State.find(:all, :include => :metro_areas, :conditions => ["metro_areas.id in (?)", metro_area_ids ]).uniq
   end
